9b5c7d020e4dc10d6b4f91469384cf248e15a3c9
Ruma <matteoh8@hotmail.it>
2022-11-30 13:33:34 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderPage />
+    <MainPage />
+  </div>
+</template>
+
+<script>
+import HeaderPage from './components/HeaderPage.vue';
+import MainPage from './components/MainPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainPage,
+    HeaderPage,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap';
+</style>
